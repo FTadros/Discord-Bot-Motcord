27eb4b6638023c90db369f100e0e18aff335ef28
FeloTadros <43042263+FTadros@users.noreply.github.com>
2021-10-07 00:05:25 +0000

    Initial commit

---
+++ cogs/utility.py
@@ -0,0 +1,58 @@
+import discord
+import traceback
+from discord.ext import commands
+import time
+import random
+import os 
+import sys
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_command_error(self, ctx, error):
+        print('Error in {0.command.qualified_name}: {1}'.format(ctx, error))
+
+    @commands.command(name = 'wiki', help = 'returns wiki link')
+    async def wiki(self, ctx, *arg):
+        arg = '_'.join(arg)
+        response = ('https://en.wikipedia.org/wiki/' + arg) 
+        await ctx.send(response)
+    
+    @commands.command(name = 'roll', help = 'ask this a question')
+    async def roll(self, ctx, *arg):
+        roll_quotes = [
+        "Yes", "No" "Dont be stupid, stupid", "Why not",
+        "Sounds ok, I guess",
+        "Are u dumb or are u stupid?", "No?",
+        "Lol? Nice joke?",
+        "You do not look handsome! Loser!",
+        "You must have inherited your mother’s genes! Loser!",
+        "It must be lonely at the bottom, huh?", "LOSER!",
+        "You are very ugly! Loser!", "Stop talking, idiot",
+        "My theory was correct.",
+        "Born a loser, dying one.",
+        "Would a retard know theyre retarded? If not, now u know.",
+        "The worst type of pain is when you’re smiling just to stop the tears from falling", ":sad:"
+        ] 
+        response = (random.choice(roll_quotes))
+        await ctx.send(response)
+    
+    @commands.command(name = 'shutdown', help = 'disconnects', aliases=['quit'], hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def shutdown(self, ctx):
+        await ctx.send('Deuces, Logging out of Pain and Suffering')
+        await self.bot.logout()
+
+    # @commands.command(hidden=True)
+    # @commands.has_permissions(administrator=True)
+    # async def restart(self, ctx):
+    #     await ctx.send('OK, restarting')       
+
+    @commands.command(name = 'hello', help = 'says hi to user', aliases=['hi', 'wsp', 'hey'])
+    async def hi(self, ctx):
+        hi_quotes = [
+            'hi', 'no', 'xd?', 'SUP SUP'
+        ]
+        response =  (random.choice(hi_quotes))
+        await ctx.send(response)
---
+++ main.py
@@ -0,0 +1,78 @@
+import discord
+import traceback
+from discord.ext import commands
+import time
+import random
+from cogs.utility import Utility
+import cogs.help
+import sys
+import os
+
+my_secret = os.environ['TOKEN']
+
+description = '''Pain and Suffering discord bot, developed with discord.py, @notfelo\n'''
+
+felo = 211639706444627969
+
+class Pain(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='*', description=description)
+    
+    async def on_ready(self):
+        if self.user.id == 772683777503789066:
+            self.me = True
+        else:
+            self.me = False
+        
+        '''logger.info('Logged in.')
+        logger.info(f'Bot-Name: {self.user.name} | ID: {self.user.id}')
+        logger.info(f'Dev Mode: {self.me}, Felo: {felo}')
+        logger.info('------')'''
+        print('WE ARE IN')
+        send_felo = await self.fetch_user(211639706444627969)
+        #cogs
+        await bot.change_presence(status = discord.Status.dnd, activity = discord.Game(name="This game called 'Life', but not for long | *help"))
+        self.add_cog(Utility(self))
+        self.add_cog(cogs.help.Help(self))
+        self.help_command = cogs.help.HelpCommand()
+
+    async def on_message(self, message):
+        if 'dababy' in message.clean_content.lower():
+            await message.add_reaction("👶")
+        await self.process_commands(message)
+
+    async def on_error(self, event, *args, **kwargs):
+        if self.me:
+            traceback.print_exc()
+        else:
+            embed = discord.Embed(title=':x: Event Error', colour=0xe74c3c) #Red
+            embed.add_field(name='Event', value=event)
+            embed.description = '```py\n%s\n```' % traceback.format_exc()
+            try:
+                await send_felo.send(embed=embed)
+            except:
+                pass
+    
+    async def on_command_error(self, error, ctx):
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.channel.send(':x:  This command has been deactivated')
+        elif isinstance(error, commands.CommandInvokeError):
+            if self.dev:
+                raise error
+            else:
+                embed = discord.Embed(title=':x: Command Error', colour=0x992d22) #Dark Red
+                embed.add_field(name='Error', value=error)
+                embed.add_field(name='Guild', value=ctx.guild)
+                embed.add_field(name='Channel', value=ctx.channel)
+                embed.add_field(name='User', value=ctx.author)
+                embed.add_field(name='Message', value=ctx.message.clean_content)
+                try:
+                    await send_felo.send(embed=embed)
+                except:
+                    pass
+    
+
+bot = Pain()
+bot.run(my_secret)
